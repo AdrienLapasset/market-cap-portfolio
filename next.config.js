9d5575eba0cf10a93eac81d9286ffa67fe9840fc
AdrienLapasset <aflapasset@gmail.com>
2023-05-20 14:30:57 +0200

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // templatePaths: ["./pages", "./components", "./tailwind"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
